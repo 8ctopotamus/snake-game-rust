a896ee8f0bf153fc89bf5ae7bba76100d0c2748f
Josh <zylo.codes@gmail.com>
2023-06-11 16:26:25 -0500

    game update method

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
+use piston::EventLoop;
 use piston::event_loop::{EventSettings, Events};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::window::WindowSettings;
 
+enum Direction {
+    Right, Left, Up, Down
+}
+
 struct Game {
     gl: GlGraphics,
-    snake: Snake
+    snake: Snake,
 }
 
 impl Game {
@@ -28,27 +33,41 @@ impl Game {
         self.snake.render(&mut self.gl, args);
     }
 
-    // fn update(&mut self, args: &UpdateArgs) {
-        
-    // }
+    fn update(&mut self, args: &UpdateArgs) {
+        self.snake.update()
+    }
 }
 
 struct Snake {
     pos_x: i32,
     pos_y: i32,
+    dir: Direction,
 }
 
 impl Snake {
     fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
         let RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = graphics::rectangle::square(self.pos_x as f64, self.pos_y as f64, 20_f64);
+        let square = graphics::rectangle::square(
+            ( self.pos_x * 20 ) as f64, 
+            ( self.pos_y * 20 ) as f64, 
+            20_f64
+        );
 
         gl.draw(args.viewport(), |c, gl| {
             let transform = c.transform;
 
             graphics::rectangle(RED, square, transform, gl);
         });
+    }
+
+    fn update(&mut self) {
+        match self.dir {
+            Direction::Left => self.pos_x -= 1,
+            Direction::Right => self.pos_x += 1,
+            Direction::Up => self.pos_x -= 1,
+            Direction::Down => self.pos_x = 1,
+        }
     }
 }
 
@@ -62,8 +81,9 @@ fn main() {
         .unwrap();
 
     let snake = Snake {
-        pos_x: 50,
-        pos_y: 100,
+        pos_x: 0,
+        pos_y: 0,
+        dir: Direction::Right,
     };
 
     let mut game = Game {
@@ -71,14 +91,14 @@ fn main() {
         snake,
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut events = Events::new(EventSettings::new()).ups(8);
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             game.render(&args);
         }
 
-        // if let Some(args) = e.update_args() {
-        //     game.update(&args);
-        // }
+        if let Some(args) = e.update_args() {
+            game.update(&args);
+        }
     }
 }
